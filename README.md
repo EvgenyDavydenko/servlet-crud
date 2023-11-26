da1c0d0e3884e2298ba27f6b872a05dc5c6fe1e6
EvgenyDavydenko <frontend82@mail.ru>
2023-11-26 12:26:40 +0300

    servlet crud

---
+++ README.md
@@ -0,0 +1,10 @@
+## JSP, Servlet, JDBC and MySQL Database: Build Todo App
+
+### Build & Run 
+
+```
+  mvn clean install && mvn --projects backend spring-boot:run
+```
+
+
+[tutorial](https://www.javaguides.net/2019/10/build-todo-app-using-jsp-servlet-jdbc-and-mysql.html)
---
+++ src/main/java/crud/UserController.java
@@ -0,0 +1,53 @@
+package crud;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class UserController extends HttpServlet {
+    private UserDao userDao;
+
+    public void init() {
+        userDao = new UserDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UserEntity employee = new UserEntity();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setUsername(username);
+        employee.setPassword(password);
+
+        try {
+            int result = userDao.registerEmployee(employee);
+            if (result == 1) {
+                request.setAttribute("NOTIFICATION", "User Registered Successfully!");
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/");
+        dispatcher.forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("jsp/register.jsp");
+    }
+
+}
